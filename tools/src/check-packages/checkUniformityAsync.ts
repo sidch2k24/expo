079997cca45cd0e9141d5efdfbaa5d7ae97f0e6a
Kudo Chien <kudo@expo.dev>
2025-11-20 04:06:13 +0800

    [tools] fix et cp clrf on windows

--- tools/src/check-packages/checkUniformityAsync.ts
+++ tools/src/check-packages/checkUniformityAsync.ts
@@ -1,3 +1,4 @@
+import os from 'os';
 import path from 'path';
 
 import { EXPO_DIR } from '../Constants';
@@ -11,6 +12,19 @@ import { spawnAsync } from '../Utils';
  * @param match Path or pattern of the files to match
  */
 export default async function checkUniformityAsync(pkg: Package, match: string): Promise<void> {
+  if (os.platform() === 'win32') {
+    // On Windows, we touch the files to force git for the CRLF -> LF conversion.
+    // This is a workaround for the fact that Git on Windows does not automatically convert CRLF to LF.
+    // warning: in the working copy of 'build/test.d.ts', CRLF will be replaced by LF the next time Git touches it
+    await spawnAsync('git', ['add', '-u', match], {
+      stdio: 'ignore',
+      cwd: pkg.path,
+    });
+    await spawnAsync('git', ['reset', match], {
+      stdio: 'ignore',
+      cwd: pkg.path,
+    });
+  }
   const child = await spawnAsync('git', ['status', '--porcelain', match], {
     stdio: 'pipe',
     cwd: pkg.path,
